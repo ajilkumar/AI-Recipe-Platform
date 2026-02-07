88391b6a9add2cb78ea2371ee9264e3db27cac88
ajilkumar <ajilk0325@gmail.com>
2026-02-07 16:10:40 +0530

    User Authentication - Implemented user authentication using Clerk - Added sign-in and sign-up pages - Updated the Header component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { neobrutalism } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -11,21 +13,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased`}>
-        {/* Header */}
-        <Header />
+    <ClerkProvider appearance={{ baseTheme: neobrutalism }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable} font-sans antialiased`}>
+          {/* Header */}
+          <Header />
 
-        {/* Main content */}
-        <main className="min-h-screen">{children}</main>
+          {/* Main content */}
+          <main className="min-h-screen">{children}</main>
 
-        {/* Footer */}
-        <footer className="py-8 px-4 border-t">
-          <div className="max-w-6xl mx-auto flex justify-center items-center">
-            <p className="text-stone-500 text-sm">Made by AJIL</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          {/* Footer */}
+          <footer className="py-8 px-4 border-t">
+            <div className="max-w-6xl mx-auto flex justify-center items-center">
+              <p className="text-stone-500 text-sm">Made by AJIL</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
