a8be2b519cf4fc19f8bc05ed6ffb7f0ec28d9cb6
ajilkumar <ajilk0325@gmail.com>
2026-02-12 23:57:13 +0530

    Created the dashboard page UI

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "api.dicebear.com",
       },
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
     ],
   },
 };
